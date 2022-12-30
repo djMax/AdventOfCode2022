0b741c64f11a375b5b8147cf4ccccd00038bfe53
Max Metral <max@pyralis.com>
2022-12-30 13:56:06 -0500

    Cleanup data out of program files

--- AdventOfCode2022/Day19/Blueprint.cs
+++ AdventOfCode2022/Day19/Blueprint.cs
@@ -4,70 +4,70 @@ using AdventOfCode2022.Search;
 
 namespace AdventOfCode2022.Day19
 {
-	public class Blueprint: World
-	{
-		static Regex COSTS = new Regex(@"Each (.*) robot costs (.*)");
+    public class Blueprint : World
+    {
+        static Regex COSTS = new Regex(@"Each (.*) robot costs (.*)");
 
-		public int Index { get; }
-		public int Minutes { get; }
+        public int Index { get; }
+        public int Minutes { get; }
 
-		public Dictionary<Resource, Dictionary<Resource, int>> Costs;
+        public Dictionary<Resource, Dictionary<Resource, int>> Costs;
 
-		public Dictionary<Resource, int> MaxRobots;
+        public Dictionary<Resource, int> MaxRobots;
 
-		public Blueprint(int index, int minutes, Dictionary<Resource, Dictionary<Resource, int>> costs, Dictionary<Resource, int> max)
-		{
-			this.Index = index;
-			this.Minutes = minutes;
-			this.Costs = costs;
-			this.MaxRobots = max;
-		}
-
-		public Blueprint WithMinutes(int m)
-		{
-			return new Blueprint(this.Index, m, this.Costs, this.MaxRobots);
-		}
-
-		public static Blueprint Parse(string input)
-		{
-			var rMap = new Dictionary<string, Resource> {
-				{ "ore", Resource.Ore },
-				{ "clay", Resource.Clay },
-				{ "obsidian", Resource.Obsidian },
-				{ "geode", Resource.Geode }
-			};
-			var s1 = input.Split(":");
-			var index = int.Parse(s1[0].Split(" ")[1]);
-
-			var costs = new Dictionary<Resource, Dictionary<Resource, int>>();
-			var max = RobotState.getResources(0);
-
-			s1[1].Trim().Split(".").ToList().ForEach(costExp =>
-			{
-				if (String.IsNullOrWhiteSpace(costExp))
-				{
-					return;
-				}
-				var spec = COSTS.Match(costExp.Trim());
-				var type = rMap[spec.Groups[1].Captures[0].Value];
-				var cost = RobotState.getResources(0);
-				costs[type] = cost;
-				spec.Groups[2].Captures[0].Value.Split(" and ").ToList().ForEach(c =>
-				{
-					var parts = c.Split(" ");
-					var resource = rMap[parts[1]];
-					var count = int.Parse(parts[0]);
-					max[resource] = max.ContainsKey(resource) ? Math.Max(count, max[resource]) : count;
-					cost[resource] = count;
-				});
-			});
-			return new Blueprint(index, 0, costs, max);
+        public Blueprint(int index, int minutes, Dictionary<Resource, Dictionary<Resource, int>> costs, Dictionary<Resource, int> max)
+        {
+            this.Index = index;
+            this.Minutes = minutes;
+            this.Costs = costs;
+            this.MaxRobots = max;
         }
 
-		public static Blueprint[] ParseAll(string input)
-		{
-			return input.Split("\n").ToList().Select(str => Parse(str)).ToArray();
-		}
+        public Blueprint WithMinutes(int m)
+        {
+            return new Blueprint(this.Index, m, this.Costs, this.MaxRobots);
+        }
+
+        public static Blueprint Parse(string input)
+        {
+            var rMap = new Dictionary<string, Resource> {
+                { "ore", Resource.Ore },
+                { "clay", Resource.Clay },
+                { "obsidian", Resource.Obsidian },
+                { "geode", Resource.Geode }
+            };
+            var s1 = input.Split(":");
+            var index = int.Parse(s1[0].Split(" ")[1]);
+
+            var costs = new Dictionary<Resource, Dictionary<Resource, int>>();
+            var max = RobotState.getResources(0);
+
+            s1[1].Trim().Split(".").ToList().ForEach(costExp =>
+            {
+                if (String.IsNullOrWhiteSpace(costExp))
+                {
+                    return;
+                }
+                var spec = COSTS.Match(costExp.Trim());
+                var type = rMap[spec.Groups[1].Captures[0].Value];
+                var cost = RobotState.getResources(0);
+                costs[type] = cost;
+                spec.Groups[2].Captures[0].Value.Split(" and ").ToList().ForEach(c =>
+                {
+                    var parts = c.Split(" ");
+                    var resource = rMap[parts[1]];
+                    var count = int.Parse(parts[0]);
+                    max[resource] = max.ContainsKey(resource) ? Math.Max(count, max[resource]) : count;
+                    cost[resource] = count;
+                });
+            });
+            return new Blueprint(index, 0, costs, max);
+        }
+
+        public static Blueprint[] ParseAll(string input)
+        {
+            return input.Split("\n").ToList().Select(str => Parse(str)).ToArray();
+        }
     }
 }
 
---
+++ AdventOfCode2022/Util/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace AdventOfCode2022.Util
+{
+	public sealed class File
+	{
+		public static string Read(string resourceName)
+		{
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+	}
+}
+
